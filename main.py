c8e8a6fd4b4d11e6246baf65a684140736100037
Owen Colley <owen.h.colley@gmail.com>
2024-09-03 17:16:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ Purpose: A digit classifier made in Python using a neural network.
 Owen Colley
 9/3/24
 '''
+
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
+
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
