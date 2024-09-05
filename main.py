00becb78bd846e65388c45d4764fafb011d600a4
Owen Colley <owen.h.colley@gmail.com>
2024-09-05 12:53:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ Owen Colley
 9/3/24
 '''
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
-
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
-print(output)
+def main():
+  inputs = [1.2, 5.1, 2.1]
+  weights = [3.1, 2.1, 8.7]
+  bias = 3
+  
+  output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+  print(output)
