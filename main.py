603b963010469d8da18032edc318bfb118fcfaef
Owen Colley <owen.h.colley@gmail.com>
2024-09-06 17:56:13 +0000

    committing work

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ Owen Colley
 '''
 
 def main():
-  inputs = [1.2, 5.1, 2.1]
-  weights = [3.1, 2.1, 8.7]
-  bias = 3
+  inputs = [1, 2, 3]
+  weights = [0.2, 0.8, -0.5]
+  bias = 2
   
   output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
   print(output)
 
 if __name__ == "__main__":
-    main()
+  main()
