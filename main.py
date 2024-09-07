f3de5b51ddeaf1b1a6e0b31af351f611198acfe3
Owen Colley <owen.h.colley@gmail.com>
2024-09-07 16:24:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,39 +7,15 @@ Owen Colley
 
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-np.random.seed(0)
-
-def create_data(points, classes):
-    X = np.zeros((points*classes, 2))
-    y = np.zeros(points*classes, dtype = 'uint8')
-    for class_number in range(classes):
-        ix = range(points*class_number, points*(classnumber + 1)
-        r = np.linspace(0.0, 1, points) # radius
-        t = np.linspace(class_number*4, (class_number + 1)*4, points) + np.random.randn(points)*0.2
-        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-        y[ix] = class_number
-    return X, y
-
-import matplotlib.pyplot as plt
-
-print("here")
-X, y = create_data(100, 3)
-
-plt.scatter(X[:,0], X[:,1])
-plt.show()
-
-plt.scatter(X[:,0], X[:,1], c = y, cmap = "brg")
-plt.show()
-
 X = [[1, 2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -52,10 +28,8 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+layer1 = Layer_Dense(2,5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-#print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
