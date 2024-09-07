79e69b50e0a347e50165c412adfee920a0e4327d
Owen Colley <owen.h.colley@gmail.com>
2024-09-07 16:12:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,40 @@ Owen Colley
 '''
 
 import numpy as np
+import nnfs
+
+nnfs.init()
 
 np.random.seed(0)
+
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype = 'uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(classnumber + 1)
+        r = np.linspace(0.0, 1, points) # radius
+        t = np.linspace(class_number*4, (class_number + 1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+import matplotlib.pyplot as plt
+
+print("here")
+X, y = create_data(100, 3)
+
+plt.scatter(X[:,0], X[:,1])
+plt.show()
+
+plt.scatter(X[:,0], X[:,1], c = y, cmap = "brg")
+plt.show()
 
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -19,6 +47,10 @@ class Layer_Dense:
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
 layer1 = Layer_Dense(4,5)
 layer2 = Layer_Dense(5,2)
