6dbbc9a8557c9034a0dcef58da5f8af6b5296ac1
Owen Colley <owen.h.colley@gmail.com>
2024-09-08 17:49:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,6 @@ import math
 
 nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -29,19 +23,24 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2,5)
+class Activation_Softmax:
+     def forward(self, inputs):
+          exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
+          probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
+          self.output = probabilities
+
+X, y = spiral_data(samples = 100, classes = 3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
 
+dense1.forward(X)
+activation1.forward(dense1.output)
 
+dense2.forward(activation1.output)
+activation2.forward(dense2.out)
 
-layer_outputs = [[4.8, 1.21, 2.385],
-                 [8.9, -1.81, 0.2],
-                 [1.41, 1.051, 0.026]]
-
-exp_values = np.exp(layer_outputs)
-norm_values = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
-
-print(norm_values)
+print(activation2.output[:5])
