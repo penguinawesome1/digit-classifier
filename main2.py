2de75a6cd62b1fc656f63685e7962f181c8f9a50
Owen Colley <owen.h.colley@gmail.com>
2024-09-09 16:09:51 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -20,23 +20,31 @@ o_data = numpy.random.randn(batch_size, o_nodes)
 w1 = numpy.random.randn(i_nodes, h_nodes)
 w2 = numpy.random.randn(h_nodes, o_nodes)
 
-h_values = i_data.dot(w1)
+loss_array = numpy.array([[]])
+indices = numpy.array([[]])
 
-# Rectified Linear Unit (ReLU)
-# removes all negative values, replacing them with 0
+for i in range(1000):
+  h_values = i_data.dot(w1)
+  h_relu = numpy.maximum(h_values, 0)
+  o_data_predictions = h_relu.dot(w2)
+  
+  loss = numpy.square(o_data_predictions - o_data).sum()
+  loss_array = numpy.append(loss_array, loss)
+  indices = numpy.append(indices, i)
+  
+  grad_pred = 2 * (o_data_predictions - o_data)
+  grad_w2 = h_relu.T.dot(grad_pred)
+  grad_h_relu = grad_pred.dot(w2.T)
+  grad_h_values = grad_h_relu.copy()
+  grad_h_calues[h_values < 0] = 0
+  grad_w1 = i_data.T.dot(grad_h_values)
+  
+  w1 -= grad_w1 * 1e-4
+  w2 -= grad_w2 * 1e-4
 
-h_relu = numpy.maximum(h_values, 0)
-o_data_predictions = h_relu.dot(w2)
+plt.plot(indices, loss_array)
+plt.legend(["Loss over iterations"])
+plt.show()
 
-loss = numpy.square(o_data_predictions - o_data).sum()
-
-grad_pred = 2 * (o_data_predictions - o_data)
-
-grad_w2 = h_relu.T.dot(grad_pred)
-
-grad_h_relu = grad_pred.dot(w2.T)
-
-grad_h_values = grad_h_relu.copy()
-grad_h_calues[h_values < 0] = 0
-
-grad_w1 = i_data.T.dot(grad_h_values)
+print(o_data)
+print(o_data_predictions)
