9c6ed73cf066271a008ce2c8cf73070bd00fb318
penguinawesome1 <owen.h.colley@gmail.com>
2024-09-17 21:16:30 -0400

    Update stock_analyzer.py

--- stock_analyzer.py
+++ stock_analyzer.py
@@ -49,7 +49,6 @@ batches = 10
 num_hidden_layers = 3
 learning_rate = 1e-3
 
-
 # set weight arrays to random numbers (setting to 0 could cause a dead system)
 weights = [np.random.randn(input_nodes, hidden_nodes)]
 weights.append([np.random.randn(hidden_nodes, hidden_nodes) for _ in range(num_hidden_layers - 1)])
@@ -59,7 +58,7 @@ weights.append(np.random.randn(hidden_nodes, output_nodes))
 biases = [np.zeros((1, hidden_nodes)) for _ in range(num_hidden_layers)]
 biases.append(np.zeros((1, output_nodes)))
 
-# Training loop
+# training loop
 for epoch in range(batches):
     for i in range(batch_size):
         start = (i * batch_size) + (epoch * input_nodes)
@@ -67,14 +66,14 @@ for epoch in range(batches):
         current_y = y[start : start + batch_size]
         
         # calculate output guess
+        y_pred = current_X
         pre_activations = []
         hidden_values = []
-        for layer in range(num_hidden_layers):
+        for layer in range(len(weights)):
             z = linearize(y_pred, weights[layer], biases[layer])
             pre_activations.append(z)
             y_pred = ReLU(z)
             hidden_values.append(y_pred)
-        y_pred = ReLU(linearize(y_pred, weights[num_hidden_layers], biases[num_hidden_layers]))
         
         # set gradient prediction to derivative of loss function
         grad_pred = 2 * (y_pred.T - current_y)
@@ -82,11 +81,20 @@ for epoch in range(batches):
         # find gradient weights and biases
         grad_weights = []
         grad_biases = []
-        for layer in reversed(range(num_hidden_layers)):
+        for layer in reversed(range(len(weights))):
+            # do ReLU for hidden layers, not output
+            if layer < len(weights) - 1:
+                grad_pred *= np.where(hidden_values[layer] > 0, 1, 0)
+
+            # update weight and bias gradient arrays
             grad_weights.append(np.dot(pre_activations[layer].T, grad_pred.T))
             grad_biases.append(np.sum(grad_pred, axis=0, keepdims=True))
+
+            # update grad_pred for hidden layers, not input
+            if layer > 0:
+                grad_pred = np.dot(grad_pred, weights[layer].T)
         
-    # Update weights and biases
+    # update weights and biases
     gradient_descent(learning_rate, weights, biases, grad_weights, grad_biases)
 
 print("Output Data: ", y)
