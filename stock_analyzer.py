d651f3d84c7a726d5a3b07e285d5581d9d3ddb2e
penguinawesome1 <owen.h.colley@gmail.com>
2024-09-17 20:47:08 -0400

    Update stock_analyzer.py

--- stock_analyzer.py
+++ stock_analyzer.py
@@ -24,8 +24,24 @@ def gradient_descent(learning_rate, weights, biases, grad_weights, grad_biases):
     for layer in range(len(biases)):
         biases[layer] -= learning_rate * grad_biases[layer]
 
+# import data from past 5 years of nasdaq
+df = pd.read_csv('5year_stock.csv')
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+X_day = df['Date'].dt.day.values[:-1] # excludes last point for current stock
+X_month = df['Date'].dt.month.values[:-1]
+X_year = df['Date'].dt.year.values[:-1]
+X_close = df['Close'].values[:-1]
+X_open = df['Open'].values[:-1]
+X_high = df['High'].values[:-1]
+X_low = df['Low'].values[:-1]
+
+# normalize data
+scaler = MinMaxScaler()
+X = scaler.fit_transform(np.column_stack([X_month, X_close, X_high, X_low]))
+y = df['Close'].values[1:] # excludes first point for tomorrow stock
+
 # define the size of the neural network
-input_nodes = 5
+input_nodes = X.shape[1]
 hidden_nodes = 8
 output_nodes = 1
 batch_size = 64
@@ -33,24 +49,10 @@ batches = 10
 num_hidden_layers = 3
 learning_rate = 1e-3
 
-# import data from past 5 years of nasdaq
-df = pd.read_csv('5year_stock.csv')
-df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
-X_month = df['Date'].dt.month.values[:-1] # excludes last point for current stock
-X_close = df['Close'].values[:-1]
-X_open = df['Open'].values[:-1]
-X_high = df['High'].values[:-1]
-X_low = df['Low'].values[:-1]
-y = df['Close'].values[1:] # excludes first point for tomorrow stock
-
-# Normalize input data
-scaler = MinMaxScaler()
-X = scaler.fit_transform(np.column_stack([X_month, X_close, X_open, X_high, X_low]))
 
 # set weight arrays to random numbers (setting to 0 could cause a dead system)
 weights = [np.random.randn(input_nodes, hidden_nodes)]
-for _ in range(num_hidden_layers - 1):
-  weights.append(np.random.randn(hidden_nodes, hidden_nodes))
+weights.append([np.random.randn(hidden_nodes, hidden_nodes) for _ in range(num_hidden_layers - 1)])
 weights.append(np.random.randn(hidden_nodes, output_nodes))
 
 # set bias arrays to 0
@@ -60,12 +62,11 @@ biases.append(np.zeros((1, output_nodes)))
 # Training loop
 for epoch in range(batches):
     for i in range(batch_size):
-        start = i * batch_size
+        start = (i * batch_size) + (epoch * input_nodes)
         current_X = X[start : start + batch_size]
         current_y = y[start : start + batch_size]
         
         # calculate output guess
-        y_pred = current_X.reshape(-1, input_nodes)
         pre_activations = []
         hidden_values = []
         for layer in range(num_hidden_layers):
