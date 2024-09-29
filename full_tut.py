07f2cba07b1ef303d8c7ee621ef919c3e068a53e
penguinawesome1 <owen.h.colley@gmail.com>
2024-09-29 17:43:16 -0400

    Create full_tut.py

---
+++ full_tut.py
@@ -0,0 +1,6 @@
+class Solution:
+  def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
+    for i in range(iterations):
+      d = 2 * init
+      init -= d * learning_rate
+    return round(init, 5)
